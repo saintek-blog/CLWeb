c9360e802bb33c4ab32f0a292ec55b329a5f62cf
noobcoder95 <64672556+noobcoder95@users.noreply.github.com>
2021-05-31 18:01:48 +0800

    initial_commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -60,33 +60,21 @@ const Navbar = class extends React.Component {
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Profil
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
+                Kabar Berita
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Blog pribadi
               </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
+              <Link className="navbar-item" to="/about">
+                Tentang
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Kirim masukan
+              </Link>
             </div>
           </div>
         </div>
