275c4676afcc3b5481a296211bee41211c03301d
noobcoder95 <64672556+noobcoder95@users.noreply.github.com>
2021-05-31 17:59:11 +0800

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Berita Sains, Teknologi dan Bisnis
           </h1>
         </div>
         <section className="section">
