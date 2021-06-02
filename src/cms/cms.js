07a40021b47ddde6d8e4fc0a5afa49483311b3ef
noobcoder95 <64672556+noobcoder95@users.noreply.github.com>
2021-06-02 18:25:43 +0800

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,6 +3,7 @@ import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import NoobPostPreview from './preview-templates/NoobPostPreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
@@ -14,3 +15,4 @@ CMS.registerPreviewTemplate('index', IndexPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('noob', NoobPostPreview)
